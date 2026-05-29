d3195d43d37fe121ba330d732efdce2df179994e
Sitanshu Anmol <sitanshu.anmol@gmail.com>
2026-05-30 02:13:42 +0530

    qwen3_scratch

---
+++ configs.py
@@ -0,0 +1,38 @@
+import os
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    d_model: int = 384
+    n_heads: int = 8
+    n_layers: int = 6
+    d_ff: int = 1536
+    n_kv_heads: int = 4
+
+    vocab_size: int = 32000
+    max_seq_len: int = 512
+    sliding_window: int = 4096
+
+    batch_size: int = 24
+    max_steps: int = 500
+    gradient_accumulation_steps: int = 4
+    weight_decay: float = 0.1
+    dropout: float = 0.1
+    grad_clip: float = 1.0
+    use_amp: bool = True
+
+    attn_bias: bool = False
+    rms_norm_eps: float = 1e-6
+    
+    n_experts: int = 128
+    n_active_experts: int = 8
+    moe_expert_hidden_dim: int = 1536
+    moe_aux_loss_coef: float = 0.01
+    tie_embeddings: bool = False
+
+    def __post_init__(self):
+        assert self.d_model % self.n_heads == 0
+        assert self.n_heads % self.n_kv_heads == 0
+
+        self.d_k = self.d_model // self.n_heads
+        self.n_kv_groups = self.n_heads // self.n_kv_heads
